570c580db79a7b180983c023f4f94b938a7ea9d9
hoodielive <hoodielive@gmail.com>
2020-02-14 16:54:14 -0500

    Updated class.py

--- classes/classes.py
+++ classes/classes.py
@@ -1,24 +1,26 @@
-class Rectangle;
+class Rectangle:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-    
+
     def area(self):
         return self.width * self.height
 
     def perimeter(self):
-
         return 2 * (self.width + self.height)
 
     def __str__(self):
+        pass
 
     def __repr__(self):
-        j
+        pass
+
     def __lt__(self):
+        pass
 
     def __gt__(self):
+        pass
 
 r1 = Rectangle(10, 20)
-r1.width
-
-
+print(r1.width)
+print(r1.perimeter)

e8380e02712e9585239c383be787dc4e2ccb7e79
hoodielive <hoodielive@gmail.com>
2020-02-13 19:11:53 -0500

    lets finish this class.. or not

--- classes/classes.py
+++ classes/classes.py
@@ -7,7 +7,16 @@ class Rectangle;
         return self.width * self.height
 
     def perimeter(self):
+
         return 2 * (self.width + self.height)
+
+    def __str__(self):
+
+    def __repr__(self):
+        j
+    def __lt__(self):
+
+    def __gt__(self):
 
 r1 = Rectangle(10, 20)
 r1.width
